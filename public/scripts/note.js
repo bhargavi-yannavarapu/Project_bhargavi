ae0499b716d68029eb2316c1677cbf4a5a307c63
bhargavi-yannavarapu <bhargaviyannavarapu@gmail.com>
2022-12-16 19:07:41 -0500

    update notes

--- public/scripts/note.js
+++ public/scripts/note.js
@@ -1,9 +1,8 @@
-import { fetchData, setCurrentUser } from './main.js'
+import { fetchData, getCurrentUser, setCurrentUser } from './main.js'
 
 // user class
 class User {
-  constructor(userID, noteContent) {
-    this.userID = userID;
+  constructor(noteContent) {
     this.noteContent = noteContent;
   }
 }
@@ -12,16 +11,26 @@ class User {
 let noteForm = document.getElementById("note_form");
 if(noteForm) noteForm.addEventListener('submit', saveNote);
 
+let user1 = getCurrentUser();
+console.log(user1)
+
+let userID = user1.userID;
+console.log(userID);
+
 function saveNote(e) {
   e.preventDefault();
 
-  let userID = document.getElementById("userID").value;
+  //let userID = document.getElementById("userID").value;
   let noteContent = document.getElementById("note").value;
-  let note = new User(userID, noteContent);
+  let note = new User(noteContent);
+  let note_ = note.noteContent;
+  let user_ = user1.userID
+  let data1 = {user_,note_}
 
-  fetchData("/notes/insert", note, "POST")
-  .then((data) => {
-    setCurrentUser(data);
+  console.log(data1)
+
+  fetchData("/notes/insert", data1, "POST")
+  .then((data1) => {
     window.location.replace = "login.html"
   })
   .catch((err) => {
